e05572ea67e69bb2f10f309134a911461cc20f05
Chlna6666 <18028376172@sina.cn>
2024-01-15 21:52:41 +0800

    测试

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,55 @@
 use std::env;
 use std::io;
 use std::fs;
+use std::fs::OpenOptions;
 use std::io::{Read, Write};
 use std::path::Path;
 use serde_json;
+use chrono::{DateTime, Utc, TimeZone, Local};
 
 const CONFIG_FILE: &str = "config.json";
 
 const JSON_DATA: &str = r#"
     {
-        "language": "en"
+        "language": "en",
+        "debug":"false"
     }
 "#;
+
+enum LogLevel {
+    Info,
+    Warning,
+    Error,
+    Debug,
+}
+
+
+fn log(level: LogLevel, message: &str) {
+    let now: DateTime<Utc> = Utc::now();
+    let local_now = now.with_timezone(&Local);
+
+    let timestamp = local_now.format("%Y-%m-%d %H:%M:%S");
+
+    let log_level_str = match level {
+        LogLevel::Info => "INFO",
+        LogLevel::Warning => "WARNING",
+        LogLevel::Error => "ERROR",
+        LogLevel::Debug => "DEBUG",
+    };
+
+    println!("[{}] [{}] {}", timestamp, log_level_str, message);
+
+    // 写入文件
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("logs.log")
+        .expect("Unable to open log file");
+
+    writeln!(&mut file, "[{}] [{}] {}", timestamp, log_level_str, message)
+        .expect("Unable to write to log file");
+}
+
 
 fn read_config_file() -> Result<(), Box<dyn std::error::Error>> {
     let path = Path::new(CONFIG_FILE);
@@ -38,9 +76,13 @@ fn create_config_file() -> Result<(), io::Error> {
 
 
 fn main() {
+    log(LogLevel::Info, "This is an information message.");
+    log(LogLevel::Warning, "This is a warning message.");
+    log(LogLevel::Error, "This is an error message.");
+    log(LogLevel::Debug, "This is a debug message.");
     // 检查命令行参数
     match read_config_file() {
-        Ok(_) => println!("配置文件存在且格式正确。"),
+        Ok(_) => log(LogLevel::Debug, "配置文件存在且格式正确。"),
         Err(err) => println!("读取或解析配置文件时出错：{:?}", err),
     }
     let args: Vec<String> = env::args().collect();
